d736f1aabc99a309338f6cf2b0bd037b626dcf98
Henry Xu <henry.m.xu@live.com>
2018-10-28 12:21:31 -0400

    Add initial examples

--- example_test.go
+++ example_test.go
@@ -14,4 +14,23 @@ func ExampleClient_GetConference() {
 	client := gonhl.NewClient()
 	conference := client.GetConference(6)
 	fmt.Println(conference.Name)
+
+	// Output: Eastern
 }
+
+func ExampleClient_GetDivision() {
+	client := gonhl.NewClient()
+	division := client.GetDivision(18)
+	fmt.Println(division.Name)
+
+	// Output: Metropolitan
+}
+
+func ExampleClient_GetDraft() {
+	client := gonhl.NewClient()
+	draft := client.GetDraft(2018)
+	fmt.Println(draft[0].DraftYear)
+
+	// Output: 2018
+}
+
